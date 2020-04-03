2a06cad3720fd2b738c369a899ef5aaadf804254
Andrei Grigortsev <grigortsevandrei@gmail.com>
2020-04-03 09:53:05 +0300

    add create subject

--- src/subjects/subjects.controller.ts
+++ src/subjects/subjects.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { SubjectsService } from './subjects.service'
+import { Roles } from '../auth/decorators/roles.decorator'
+import { UserRolesType } from '../users/user.entity'
+import { RolesGuard } from '../auth/roles.guard'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { Subject } from './subject.entity'
+import { CreateSubjectDto } from './dto/createSubject.dto'
 
 @ApiTags('colleges')
 @Controller('subjects')
-export class SubjectsController {}
+export class SubjectsController {
+  constructor(private readonly subjectsService: SubjectsService) {}
+
+  @ApiBearerAuth()
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Roles(UserRolesType.USER)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Body() createSubjectDto: CreateSubjectDto): Promise<Subject> {
+    return await this.subjectsService.create(createSubjectDto)
+  }
+}
--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common'
+import { CreateSubjectDto } from './dto/createSubject.dto'
+import { Subject } from './subject.entity'
+import { BadRequestExceptionError } from '../tools/exceptions/BadRequestExceptionError'
 
 @Injectable()
-export class SubjectsService {}
+export class SubjectsService {
+  async create(createSubjectDto: CreateSubjectDto): Promise<Subject> {
+    try {
+      const subject = await Subject.create(createSubjectDto).save()
+
+      return await this.findOne(subject.id)
+    } catch (e) {
+      if (e.name === 'QueryFailedError' && e.code === 'ER_DUP_ENTRY') {
+        throw new BadRequestExceptionError({
+          property: 'field',
+          value: '',
+          constraints: {
+            duplicate: e.message,
+          },
+        })
+      }
+    }
+  }
+
+  async findOne(id: number): Promise<Subject> {
+    const subject = await Subject.findOne({
+      where: {
+        id,
+      },
+      relations: ['teachers', 'colleges', 'specialties'],
+    })
+
+    if (!subject) {
+      throw new BadRequestExceptionError({
+        property: 'id',
+        value: id,
+        constraints: {
+          isNotExist: 'subject is not exist',
+        },
+      })
+    }
+
+    return subject
+  }
+}
