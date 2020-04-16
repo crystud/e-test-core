4f0e205c315c03c448c86e9d64cfabade0073c23
Andrei Grigortsev <grigortsevandrei@gmail.com>
2020-04-16 21:53:19 +0300

    add find user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,24 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
+  Get,
+  Param,
   Post,
+  Request,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common'
 import { RegisterUserDto } from './dto/registerUser.dto'
 import { UsersService } from './users.service'
 import { AuthService } from '../auth/auth.service'
 import { TokensInterface } from '../auth/interfaces/tokens.interface'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { Roles } from '../auth/decorators/roles.decorator'
+import { User } from './user.entity'
+import { RolesGuard } from '../auth/roles.guard'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { UserRolesType } from '../enums/userRolesType'
+import { classToClass } from 'class-transformer'
 
 @ApiTags('users')
 @Controller('users')
@@ -27,5 +37,17 @@ export class UsersController {
     const user = await this.usersService.createUser(registerUserDto)
 
     return await this.authService.createTokens(user)
+  }
+
+  @ApiBearerAuth()
+  @Roles(UserRolesType.USER)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async findOne(@Param('id') userId: number, @Request() req): Promise<User> {
+    const user = await this.usersService.findOne(userId)
+    return classToClass(user, {
+      groups: req.role,
+    })
   }
 }
