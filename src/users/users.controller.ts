5c3b472ae6ff6d45e77464c22befe90617fc5480
Andrei Grigortsev <grigortsevandrei@gmail.com>
2020-04-24 22:13:37 +0300

    add find users by filter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Param,
   Post,
+  Query,
   Request,
   UseGuards,
   UseInterceptors,
@@ -26,6 +27,7 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 import { UserRolesType } from '../enums/userRolesType'
 import { classToClass } from 'class-transformer'
 import { AccessLevelType } from '../enums/accessLevelType'
+import { FilterUserDto } from './dto/filterUser.dto'
 
 @ApiTags('users')
 @Controller('users')
@@ -79,6 +81,26 @@ export class UsersController {
 
     return classToClass(user, {
       groups: [...req.user.roles, AccessLevelType.OWNER],
+    })
+  }
+
+  @ApiBearerAuth()
+  @Roles(UserRolesType.USER)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @ApiOkResponse({
+    type: [User],
+    description: 'Find users list by filter',
+  })
+  async findAll(
+    @Query() filterUserDto: FilterUserDto,
+    @Request() req,
+  ): Promise<User[]> {
+    const users = await this.usersService.findAll(filterUserDto)
+
+    return classToClass(users, {
+      groups: [...req.user.roles],
     })
   }
 }
