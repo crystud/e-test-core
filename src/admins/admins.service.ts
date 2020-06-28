97354e7c24720ea0786fc68d44ff30b711d90b53
Andrei Grigortsev <grigortsevandrei@gmail.com>
2020-06-28 15:01:39 +0300

    add makeing first user in system admin

--- src/admins/admins.service.ts
+++ src/admins/admins.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { User } from '../users/user.entity'
+import { Admin } from './admin.entity'
 
 @Injectable()
-export class AdminsService {}
+export class AdminsService {
+  async create(user: User): Promise<Admin> {
+    return Admin.create({
+      user,
+    })
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
 import { AuthModule } from '../auth/auth.module'
 import { ConfigModule } from '@nestjs/config'
+import { AdminsModule } from '../admins/admins.module'
 
 @Module({
-  imports: [AuthModule, ConfigModule],
+  imports: [AuthModule, ConfigModule, AdminsModule],
   exports: [UsersService],
   providers: [UsersService],
   controllers: [UsersController],
